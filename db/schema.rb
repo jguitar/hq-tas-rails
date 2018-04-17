0e215fcbd83226bf9526628cb1746c5c259c9634
Juan F. Pérez <j.guitar@gmail.com>
2018-04-17 11:35:54 +0200

    rails g scaffold Site code:string name:string

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_04_17_082949) do
+ActiveRecord::Schema.define(version: 2018_04_17_093506) do
 
   create_table "business_units", force: :cascade do |t|
+    t.string "code"
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "sites", force: :cascade do |t|
     t.string "code"
     t.string "name"
     t.datetime "created_at", null: false
